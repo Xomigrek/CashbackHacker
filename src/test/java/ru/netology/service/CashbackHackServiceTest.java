9392b87ee5855241996242351366ba76041f89a5
Igor Khomenko <xomigrek@gmail.com>
2022-09-05 21:50:29 +0300

    Add JUnit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest{
+    @Test
+    public void needToBuy1 () {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void needToBuy0 () {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void needToBuy999 () {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(expected, actual);
+    }
+}
